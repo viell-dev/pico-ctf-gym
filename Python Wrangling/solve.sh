4a4a3b979237950bca80e82b0160deb1630e5e8b
viell <viell.dev@gmail.com>
2022-03-12 17:36:56 +0100

    Renamed solution.sh to solve.sh

--- Python Wrangling/solve.sh
+++ Python Wrangling/solve.sh
@@ -1,23 +1,23 @@
 #/bin/bash
 
 # Check if a python env exists
-if [ -d ./env ]
+if [ -d env ]
 then
   # Activate the existing python env
-  source ./env/bin/activate
+  source env/bin/activate
 else
   # Create a new python env
   python3 -m venv env &> /dev/null
 
   # Activate the python env
-  source ./env/bin/activate
+  source env/bin/activate
 
   # Install required packages
-  pip install -r ./requirements.txt &> /dev/null
+  pip install -r requirements.txt &> /dev/null
 fi
 
 # Get the flag
-cat ./pw.txt | xargs -I {} python3 ./ende.py -d ./flag.txt.en {}
+cat pw.txt | xargs -I {} python3 ende.py -d flag.txt.en {}
 
 # Deactivate the python env
 deactivate
